682d82c39b950ac4ffd5e6af96afe2aadfb8d386
Jerom Ghagra <Jeromghagra@gmail.com>
2021-05-01 21:35:04 +0600

    update

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -2,10 +2,10 @@ import axios from "axios";
 //import auth from "./authService"
 
 axios.defaults.headers.common["x-auth-token"] = localStorage.getItem("token")
-
-export default {
+const http ={
   get: axios.get,
   post: axios.post,
   put: axios.put,
   delete: axios.delete
 };
+export default http
