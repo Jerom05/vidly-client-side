07f0cb8409e79251367b963ad897b940a78b8b9c
Jerom Ghagra <Jeromghagra@gmail.com>
2021-05-01 20:07:11 +0600

    update

--- src/components/common/table.js
+++ src/components/common/table.js
@@ -1,6 +1,6 @@
 import React from "react";
 import TableHeader from "./tableHeader";
-import TableBody from "./tableBody";
+import TableBody from './tableBody'
 
 const Table = ({ columns, sortColumn, onSort, data }) => {
   return (
